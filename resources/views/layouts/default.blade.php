bdfd343d6ea863ba1a4f6d5fea4591c4ab0ccc23
sam <yzenithcorp@gmail.com>
2017-11-04 14:42:35 +0000

    finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title','Sample') -- Sam Yan's Project</title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','about page')
+@section('content')
+	<h1>关于页</h2>
+@stop
