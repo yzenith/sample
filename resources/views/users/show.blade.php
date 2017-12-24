f6d37ea7e21f26c88ecd14d22fb11d835768e784
sam <yzenithcorp@gmail.com>
2017-12-24 18:44:59 +0000

    some change

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-@extends('layouts.default')
-=======
 @extends('layout.default')
->>>>>>> sign-up
 @section('title',$user->name)
 @section('content')
 {{ $user->name }} - {{ $user->email }}
